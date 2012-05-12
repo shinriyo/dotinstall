fd5aa3817a19b964b497f29d3ea59438ce67577b
shinriyo <shinriyo@gmail.com>
2012-05-13 08:06:10 +0900

    test

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+# coding: utf-8
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path, :notice => '作成されました!'
+    else
+      render :action => 'new'
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '9705888eca23dd4adbcc0b0c443c80975601305555ec6e9411724a4434c94693de80c8da8e2b284a7ce5def47c50264b06fd0c4135b60281ca2dda3f5bab85eb'
